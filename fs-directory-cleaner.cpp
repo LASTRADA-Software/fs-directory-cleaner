27dc3878de9d757d9219368b74156ac87b9d5745
Christian Parpart <christian@parpart.family>
2025-01-28 23:07:16 +0100

    Potential way to demo dependency injection
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- fs-directory-cleaner.cpp
+++ fs-directory-cleaner.cpp
@@ -4,9 +4,47 @@
 #include <format>
 #include <iostream>
 #include <print>
+#include <vector>
 
 namespace fs = std::filesystem;
 using namespace std::string_view_literals;
+
+class Executor
+{
+  public:
+    Executor() = default;
+    Executor(Executor&&) = default;
+    Executor& operator=(Executor&&) = default;
+    Executor(const Executor&) = delete;
+    Executor& operator=(const Executor&) = delete;
+    virtual ~Executor() = default;
+
+    virtual bool isDirectory(fs::path const& path) = 0;
+    virtual bool removeFile(fs::path const& path, std::error_code& ec) = 0;
+    virtual std::vector<fs::directory_entry> listDirectories(const fs::path& path) = 0;
+};
+
+class RealFilesystemExecutor: public Executor
+{
+  public:
+    bool isDirectory(fs::path const& path) override
+    {
+        return fs::is_directory(path);
+    }
+
+    bool removeFile(fs::path const& path, std::error_code& ec) override
+    {
+        return fs::remove(path, ec);
+    }
+
+    std::vector<fs::directory_entry> listDirectories(const fs::path& path) override
+    {
+        auto result = std::vector<fs::directory_entry>();
+        for (auto entry: fs::directory_iterator(path, fs::directory_options::skip_permission_denied))
+            result.emplace_back(std::move(entry));
+        return result;
+    }
+};
 
 enum class RunMode : bool
 {
@@ -19,12 +57,13 @@ auto const Yellow = "\033[33;1m"sv;
 auto const Red = "\033[31;1m"sv;
 auto const Green = "\033[32;1m"sv;
 
+void deleteRecursively(const fs::path& path, RunMode runMode, Executor& executor)
 {
-    if (fs::is_directory(path))
+    if (executor.isDirectory(path))
     {
-        for (const auto& entry: fs::directory_iterator(path, fs::directory_options::skip_permission_denied))
+        for (const auto& entry: executor.listDirectories(path))
         {
-            deleteRecursively(entry.path(), runMode);
+            deleteRecursively(entry.path(), runMode, executor);
         }
     }
     else
@@ -37,7 +76,7 @@ auto const Green = "\033[32;1m"sv;
             case RunMode::Execute: {
                 std::print("{}Removing:{} {}\n", Red, Reset, path.string());
                 auto ec = std::error_code {};
-                fs::remove(path, ec);
+                executor.removeFile(path, ec);
                 if (ec)
                     std::cerr << std::format("Error: {}\n", ec.message());
                 break;
@@ -48,21 +87,15 @@ auto const Green = "\033[32;1m"sv;
 
 void deleteDirectoriesIfOlderThan(const fs::path& baseDirectory,
                                   fs::file_time_type::clock::time_point oldestAllowed,
-                                  RunMode runMode = RunMode::DryRun)
+                                  RunMode runMode,
+                                  Executor& executor)
 {
-    std::println("Deleting directories older than: {} in: {}", oldestAllowed.time_since_epoch().count(), baseDirectory.string());
-    auto const reset = "\033[0m"sv;
-    auto const red = "\033[31;1m"sv;
-    auto const green = "\033[32;1m"sv;
-    for (auto const& entry: fs::directory_iterator(baseDirectory))
+    for (auto const& entry: executor.listDirectories(baseDirectory))
     {
         auto ec = std::error_code {};
         auto const lastWriteTime = entry.last_write_time();
         if (lastWriteTime.time_since_epoch() < oldestAllowed.time_since_epoch())
-        {
-            std::println("{}Deleting:{} {}", red, reset, entry.path().string());
-            deleteRecursively(entry, runMode);
-        }
+            deleteRecursively(entry, runMode, executor);
         else
             std::println("{}Skipping:{} {}", Green, Reset, entry.path().string());
     }
@@ -86,8 +119,10 @@ int main(int argc, char** argv)
     auto const minimumAge = std::chrono::minutes(std::stoi(argv[2]));
     auto const now = fs::file_time_type::clock::now();
     auto const oldestAllowed = now - minimumAge;
+    auto const runMode = RunMode::DryRun;
+    auto executor = RealFilesystemExecutor {};
 
-    deleteDirectoriesIfOlderThan(rootPath, oldestAllowed, RunMode::DryRun);
+    deleteDirectoriesIfOlderThan(rootPath, oldestAllowed, runMode, executor);
 
     return EXIT_SUCCESS;
 }
